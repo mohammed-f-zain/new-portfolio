a09ccff8bb37d95263a716ea83e2c426889d95ff
mohammed-f-zain <mohammed.f.zain@gmail.com>
2023-10-24 14:21:44 +0300

    File structure

---
+++ frontend_react/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom'; // Corrected import statement
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
